cc3975133d946815897647ab2dfa013db2f85866
Milo <camilo13777@gmail.com>
2025-07-07 21:22:22 -0300

    feat: finalizado atividade 2.

--- atividade_2/parte2.py
+++ atividade_2/parte2.py
@@ -2,4 +2,14 @@ import networkx as nx
 
 G = nx.read_gml("atividade_2/lesmis.gml")
 
-print(len(G.nodes))
+# print(f"O número de nós é {G.number_of_nodes()} e o número de arestas é {G.number_of_edges()}.")
+
+graus = nx.degree(G)
+somatorio = 0
+
+for nome, grau in graus:
+    somatorio += grau
+
+grau_medio = somatorio / G.number_of_nodes()
+
+print(f"O diâmetro do grafo é {nx.diameter(G)} e o grau médio é {grau_medio:.2f}.")
